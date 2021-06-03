1781ec9eaa967d8e0f8d3ec692aaba4a90d82f8e
dannyandreev <danny.andreev@gmail.com>
2021-06-03 15:36:57 -0700

    car drives and stops, and stops when headed out of bounds

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.addEventListener('keydown', handleKeyDown);
 
 var carImage = document.getElementById('car');
+var carLoc = document.getElementById('carDiv');
+
+var intervalID = 0;
 
 var car = {
   angle: 0,
@@ -8,12 +11,16 @@ var car = {
   position: [0, 0]
 };
 
+var backgroundSize = [2000, 1500];
+
 function handleKeyDown(event) {
   if (event.code === 'Space') {
     if (car.state === 'off') {
       car.state = 'on';
+      intervalID = setInterval(drive, 15);
     } else {
       car.state = 'off';
+      clearInterval(intervalID);
     }
   } else if (event.code === 'ArrowLeft') {
     car.angle -= 10;
@@ -21,4 +28,26 @@ function handleKeyDown(event) {
     car.angle += 10;
   }
   carImage.style.transform = 'rotate(' + car.angle + 'deg)';
+}
+
+function moveCar(distance) {
+  if (car.state === 'on') {
+    var x = Math.round(distance * Math.cos(car.angle * Math.PI / 180));
+    var y = Math.round(distance * Math.sin(car.angle * Math.PI / 180));
+
+    updatePosition(x, y);
+
+  }
+}
+
+function updatePosition(x, y) {
+  if ((car.position[0] + x < backgroundSize[0] && car.position[0] + x >= 0) && (car.position[1] + y < backgroundSize[1] && car.position[1] + y >= 0)) {
+    car.position[0] += x;
+    car.position[1] += y;
+    carLoc.style.transform = 'translate(' + car.position[0] + 'px, ' + car.position[1] + 'px)';
+  }
+}
+
+function drive() {
+  moveCar(10);
 }
